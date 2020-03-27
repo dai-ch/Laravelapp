6ba306f16ab8ab3ea08382e728684f2c1a156d7c
Daisuke Chuda <09p061@gmail.com>
2020-03-27 22:33:09 +0900

    MVCの学習中

--- routes/web.php
+++ routes/web.php
@@ -15,48 +15,29 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-$html = <<<EOF
-<html>
-<head>
-<title>Hello</title>
-<style>
-body {font-size:16pt; color:#999; }
-h1 { font-size:100pt; text-align:right; color:#eee;
-   margin:-40px 0px -50px 0px; }
-</style>
-</head>
-<body>
-   <h1>Hello</h1>
-   <p>This is sample page.</p>
-   <p>これは、サンプルで作ったページです。</p>
-</body>
-</html>
-EOF;
+// $html = <<<EOF
+// <html>
+// <head>
+// <title>Hello</title>
+// <style>
+// body {font-size:16pt; color:#999; }
+// h1 { font-size:100pt; text-align:right; color:#eee;
+//    margin:-40px 0px -50px 0px; }
+// </style>
+// </head>
+// <body>
+//    <h1>Hello</h1>
+//    <p>This is sample page.</p>
+//    <p>これは、サンプルで作ったページです。</p>
+// </body>
+// </html>
+// EOF;
 
 // Route::get('hello',function () use ($html) {
 //    return $html;
 // });
 
-Route::get('hello/{msg}/{pass?}',function ($msg,$pass='no message') {
-   $html = <<<EOF
-
-   <html>
-<head>
-<title>Hello</title>
-<style>
-body {font-size:16pt; color:#999; }
-h1 { font-size:100pt; text-align:right; color:#eee;
-   margin:-40px 0px -50px 0px; }
-</style>
-</head>
-<body>
-   <h1>Hello</h1>
-   <p>{$msg},{$pass}</p>
-   <p>これは、サンプルで作ったページです。</p>
-</body>
-</html>
-EOF;
-
-return $html;
+Route::get('hello', function(){
+    return view('hello.index');
 });
 
